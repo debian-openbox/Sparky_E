d377f97227b49cb23d532403e7c1f1bb1a63be51
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-03-31 08:51:12 +0200

    Update wps-office.sh

--- scripts/wps-office.sh
+++ scripts/wps-office.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-cd && wget -O wps-office.deb https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9719/wps-office_11.1.0.9719.XA_amd64.deb
-sudo chmod 777 /home/$(logname)/wps-office.deb
-sudo dpkg -i wps-office.deb
-sudo apt-get -f install && rm wps-office.deb
-cp -p /home/$(logname)/Bullseye_Box/scripts/install_missing_wps_fonts.sh /home/$(logname)/.scripts
-cd /home/$(logname)/.scripts
-sudo ./install_missing_wps_fonts.sh
+if [ ! -d /home/$(logname)/Reports ]; then
+    mkdir /home/$(logname)/Reports
+fi
+
+DIR=/home/$(logname)/Reports
+
+# wget output file
+FILE=wps-office.deb
+
+# wget log file
+LOGFILE=wget.log
+
+# wget download url
+URL=https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9719/wps-office_11.1.0.9719.XA_amd64.deb
+
+cd $DIR
+wget $URL -O $FILE -o $LOGFILE
+
+rm $FILE
