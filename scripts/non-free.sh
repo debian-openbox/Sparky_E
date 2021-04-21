9796c88e60bddf830cab63d5d2904ba86f5a03b0
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-04-21 15:57:09 +0200

    Update non-free.sh

--- scripts/non-free.sh
+++ scripts/non-free.sh
@@ -4,18 +4,18 @@
 #
 
 
-DEBIAN_RELEASE=`cat /etc/*-release 2> /dev/null | grep PRETTY_NAME | awk -F "=" {'print $2'} | awk -F "(" {'print $2'} | awk -F ")" {'print $1'}`
+DEBIAN_RELEASE=`awk -F"[)(]+" '/VERSION=/ {print $2}' /etc/os-release`
 
 echo "Writes /etc/apt/sources.list in order to add $DEBIAN_RELEASE non-free repository"
 
 echo "# deb http://http.debian.net/debian $DEBIAN_RELEASE main" > /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
-echo "deb http://http.debian.net/debian $DEBIAN_RELEASE main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://http.debian.net/debian $DEBIAN_RELEASE main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://http.debian.net/debian/ $(DEBIAN_RELEASE) main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://http.debian.net/debian/ $(DEBIAN_RELEASE) main contrib non-free" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
-echo "deb http://security.debian.org/ $DEBIAN_RELEASE/updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://security.debian.org/ $DEBIAN_RELEASE/updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/ $(DEBIAN_RELEASE)-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://security.debian.org/ $(DEBIAN_RELEASE)-updates main contrib non-free" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
 echo "# $DEBIAN_RELEASE-updates, previously known as "volatile"" >> /etc/apt/sources.list
-echo "deb http://http.debian.net/debian $DEBIAN_RELEASE-updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://http.debian.net/debian $DEBIAN_RELEASE-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://http.debian.net/debian $(DEBIAN_RELEASE)-security main" >> /etc/apt/sources.list
+echo "deb-src http://http.debian.net/debian $(DEBIAN_RELEASE)-security main" >> /etc/apt/sources.list
