d48fc42d239b310be329a30d9f30a4d8a66bc738
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-05-04 15:30:35 +0200

    Update autostart.sh

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,13 +1,32 @@
+#! /bin/bash
 
-## GNOME PolicyKit and Keyring
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-#eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
-
-nitrogen --restore &
-
-export PATH="${PATH}:$HOME/.scripts"
-
+sparky-polkit &
+nitrogen --restore && \
+if [ -f /usr/bin/tint2 ]; then
 tint2 &
+elif [ -f /usr/bin/fbpanel ]; then
+fbpanel &
+fi
+xscreensaver -nosplash &
+(sleep 5; pnmixer) &
+thunar --daemon &
+if [ -f /opt/sparky/nm-applet-reload ]; then
+/opt/sparky/nm-applet-reload &
+fi
+if [ -f /usr/bin/xfce4-power-manager ]; then
+/usr/bin/xfce4-power-manager &
+fi
+if [ -f /usr/bin/xdg-user-dirs-update ]; then
+/usr/bin/xdg-user-dirs-update &
+fi
+
+sleep 10 && /usr/bin/conky-session &
+
+package-update-indicator &
+
+compton -cC -i 0.85 -e 0.85 -m 0.9 -r 4 -l -4 -t -4 &
+
+numlockx on &
 
 ~/keyboard.sh
 
@@ -15,20 +34,4 @@ fbxkb &
 
 urxvtd -q &
 
-#~/conkystartup.sh
-sleep 10 && /usr/bin/conky-session &
-
-pnmixer &
-
-udiskie &
-
-package-update-indicator &
-
-compton -cC -i 0.85 -e 0.85 -m 0.9 -r 4 -l -4 -t -4 &
-# $HOME/bin/start-compton.sh &
-
-# Start screensaver
-xscreensaver -no-splash &
-
-# Turn on Numlock at start-up
-numlockx on &
+export PATH="${PATH}:$HOME/.scripts"
